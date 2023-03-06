57176eb57b55212c4dd73126c5451da3cda78fa1
Samuel Valasek <samuelvalasekcontact@gmail.com>
2023-03-06 12:54:59 +0000

    Initial Commit

---
+++ AshfieldStudyTracker/AshfieldStudyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AshfieldStudyTrackerApp.swift
+//  AshfieldStudyTracker
+//
+//  Created by Sam Valasek on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AshfieldStudyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
